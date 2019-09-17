dad1db43fc5e9743f085cf8924811df1369c22b3
buch95 <54857987+buch95@users.noreply.github.com>
2019-09-17 14:02:37 +0300

    Completed homework 1
    создать html файл, внутри форма
    с уже проставленными (атрибут value тега input) значениями внутри
    скрипт внутри файла (в конце body в html-файле создать тег <script> и внутри уже можно писать на js) должен вытащить значения этих полей (firstName, email, age) и заполнить этими значениями объект, вывести этот объект в консоль

--- Homework1/app/main.js
+++ Homework1/app/main.js
@@ -1,1 +1,8 @@
-var a = 100;
+var nameInput = document.getElementById("name");
+var dateInput = document.getElementById("date");
+var emailInput = document.getElementById("email");
+
+document.querySelector('form.wow').addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log(nameInput.value, emailInput.value, dateInput.value);
+});

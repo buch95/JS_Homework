77a4874aaa2d682c1d8d81ebdcaebcb146e6e204
buch95 <54857987+buch95@users.noreply.github.com>
2019-09-19 17:26:26 +0300

    Homework 2 unfinished
    Questions about splice

--- Homework1/app/main.js
+++ Homework1/app/main.js
@@ -1,10 +1,63 @@
-var nameInput = document.getElementById("name");
-var dateInput = document.getElementById("date");
-var emailInput = document.getElementById("email");
+let form = document.getElementsByClassName('wow');
+let userData = [];
 
+function renderUsers(userData) {
+    let htmlStr = ``;
+    for (let i in userData) {
+        htmlStr += `<tr>
+            <td>${+i + 1}</td>
+            <td>${userData[i].name}</td>
+            <td>${userData[i].email}</td>
+            <td>${userData[i].date}</td>
+            <td><img src="${userData[i].picture}"></td>
+            <td><button type="button" id="del" onclick="userData.splice(i, i)"><img src="img/cross.png"></button>
+        </tr>`
+    }
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('date').value = '';
+    document.getElementById('picture').value = '';
+    document.querySelector('tbody').innerHTML = htmlStr; 
+}
 
+// document.querySelector('form.wow').addEventListener('submit', function (e) {
+//     e.preventDefault();
+//     console.log(userCard.name.value, userCard.email.value, userCard.date.value);
+// });
 
-document.querySelector('form.wow').addEventListener('submit', function (e) {
+function addUser(e) {
     e.preventDefault();
-    console.log(nameInput.value, emailInput.value, dateInput.value);
-});
+    let userCard = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        date: document.getElementById('date').value,
+        picture: document.getElementById('picture').value
+    }
+    if (!userCard.name) {
+        alert('Please type in your name');
+        return;
+    }
+    else {
+        if (!userCard.email) {
+            alert('Please type in your email');
+            return;
+        }
+        else {
+            if (!userCard.date) {
+                alert('Please type in your date of birth');
+                return;
+            }
+            else {
+                if (!userCard.picture) {
+                    alert('Please add your picture');
+                    
+                }
+            }
+        }
+    }
+    userData.push(userCard);
+    renderUsers(userData);
+}
+document.getElementById('submit').addEventListener('click', addUser);
+
+    

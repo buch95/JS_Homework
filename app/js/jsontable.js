db57bf6ab1e24dacd6ac7bfb57cbc3ffa5f34c1b
buch95 <54857987+buch95@users.noreply.github.com>
2019-09-24 00:02:25 +0300

    Update jsontable.js
    Added ajax method

--- app/js/jsontable.js
+++ app/js/jsontable.js
@@ -1,28 +1,35 @@
-// $.ajax({
-//     url: 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json',
-//     method: 'GET',
-//     error: (e) => {
-//         console.log(e);
-//     },
-//     success: (data) => {
-//         console.log(data);
-//     }
-// });
+$.ajax({
+    url: 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json',
+    method: 'GET',
+    dataType: 'json',
+    success: (data) => {
+        $(data).each(function (i, currency) {
+                    $('#jsontb').append($("<tr>")
+                        .append($("<td>").append(currency.txt))
+                        .append($("<td>").append(currency.rate))
+                        .append($("<td>").append(currency.cc))
+                        .append($("<td>").append(currency.exchangedate)));
+                });
+    },
+    error: (e) => {
+        console.log(e);
+    }
+});
 
-$.getJSON('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json', function (data) {
-    // esli vivesti v consol: console.log(data)
-    $(data).each(function (i, currency) {
-        $('#jsontb').append($("<tr>")
-            .append($("<td>").append(currency.txt))
-            .append($("<td>").append(currency.rate))
-            .append($("<td>").append(currency.cc))
-            .append($("<td>").append(currency.exchangedate)));
-    });
-})
-    .done(function () {
-        console.log("Completed");
-    })
-    .fail(function (e) {
-        console.log('error:');
-        console.error(e);
-    });
+// $.getJSON('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json', function (data) {
+//     // esli vivesti v consol: console.log(data)
+//     $(data).each(function (i, currency) {
+//         $('#jsontb').append($("<tr>")
+//             .append($("<td>").append(currency.txt))
+//             .append($("<td>").append(currency.rate))
+//             .append($("<td>").append(currency.cc))
+//             .append($("<td>").append(currency.exchangedate)));
+//     });
+// })
+//     .done(function () {
+//         console.log("Completed");
+//     })
+//     .fail(function (e) {
+//         console.log('error:');
+//         console.error(e);
+//     });
